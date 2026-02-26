1bd1dad7f116fa9e176cca42487bdcc5ec408e46
Felix Cruz <fcruzm95@gmail.com>
2026-02-26 17:21:08 +0100

    Added test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,18 @@ export default mergeConfig(
       include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'],
       exclude: ['src/**/*.browser.{test,spec}.{js,ts}', 'node_modules'],
       setupFiles: './src/test/setup.ts',
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'html', 'json'],
+        reportsDirectory: './coverage',
+        exclude: [
+          'node_modules/',
+          'src/test/',
+          '**/*.d.ts',
+          '**/*.config.{js,ts}',
+          '**/types/**',
+        ],
+      },
     },
   })
 )
