35d7fa6f13cfe262bb5f36f2c618dfde624ee12a
Felix Cruz <fcruzm95@gmail.com>
2026-03-01 12:24:05 +0100

    removed unused files

--- src/domain/PokedexEntry.ts
+++
@@ -1,8 +0,0 @@
-export interface PokemonSpecies {
-  // generation: number; // TODO: Add when updating for multiple generations
-  pokedexNumber: number;
-  name: string;
-  sprite: string;
-  exclusiveGame: "red" | "blue" | null;
-  // caughtLocations: any[]; // TODO: Update with caught locations by game by pokemon when adding location.
-}
